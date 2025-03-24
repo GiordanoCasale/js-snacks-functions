014d0fecfd566684f0b53175b6506799a4b0fd40
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-24 16:47:15 +0100

    [DONE] Bonus completed

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,24 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function day(username){
+    const time = new Date();
+    let hour = time.getHours();
+    if(hour<=13){
+        console.log("buongiorno " + username)
+    }
 
+    else if(hour<=17){
+        console.log("buon pomeriggio " + username)
+    }
+
+    else{
+        console.log("buonasera " + username)
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+day(name)
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
