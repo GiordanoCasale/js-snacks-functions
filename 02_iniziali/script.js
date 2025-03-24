7357c9d2d17a8a3cb0fe6512344578bc97eea917
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-24 14:39:59 +0100

    [UPD] Function exercise 2 created

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,7 +4,9 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function nameList(arr){
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
