a4ac1273bb91669bb9e35557048d68157fad1ef1
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-24 15:25:23 +0100

    [UPD] Exercise 3 also completed with arrow function

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -19,9 +19,26 @@ function nameList(arr) {
     return result
 }
 
+const arrList=(arr)=>{
+    let result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].charAt(0) === "A") {
+            result.push(arr[i])
+        }
+
+
+    }
+
+
+
+    return result
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 console.log(nameList(names))
+console.log(arrList(names))
+
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
