095dcee1fb0bc1095c8e592b54aabaf8c06f9170
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-24 16:23:53 +0100

    [UPD] completdd exercise 4 also with arrow function

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -17,8 +17,21 @@ function vocal(letters){
     return result
 }
 
+const vocal2=(letters)=>{
+    let count = 0
+    let result = []
+    for(let i=0; i<letters.length; i++){
+        if(letters[i] === "a" || letters[i] === "e" || letters[i] === "i" || letters[i] === "o" || letters[i] === "u" ){
+            count++
+            result.push(letters[i])
+        }
+    }
+
+    return result
+}
+
 // Invoca la funzione qui e stampa il risultato in console
 console.log(vocal(word).length + "(" + vocal(word) + ")");
-
+console.log(vocal2(word).length + "(" + vocal2(word) + ")");
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
